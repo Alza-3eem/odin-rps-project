8fdbcce9676e1e3848db65576ebb3bad6b053ae8
Alza-3eem <176985194+Alza-3eem@users.noreply.github.com>
2024-08-11 15:04:34 -0400

    modified the rps game to display game winners after one of the scores get to 5

--- script.js
+++ script.js
@@ -3,22 +3,42 @@ const paperBtn = document.getElementById("paper");
 const scissorsBtn = document.getElementById("scissors");
 const result = document.getElementById("result");
 
-rockBtn.addEventListener("click", () => play('rock'));
-paperBtn.addEventListener("click", () => play('paper'));
-scissorsBtn.addEventListener("click", () => play('scissors'));
+rockBtn.addEventListener("click", () => playRound('rock'));
+paperBtn.addEventListener("click", () => playRound('paper'));
+scissorsBtn.addEventListener("click", () => playRound('scissors'));
 
 const getComputerChoice = () => {
     const choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() * choices.length)]
 };
 
-const play = (userChoice) => {
+let playerScore = 0
+let computerScore = 0
+
+const playRound = (userChoice) => {
     const computerChoice = getComputerChoice();
+
+    let roundResult;
+
     if (userChoice === computerChoice) {
-        return result.innerText = `It was a tie! Both chose ${userChoice}`
+        roundResult = `It was a tie! Both chose ${userChoice}.`
     } else if ((userChoice === 'rock' && computerChoice === 'scissors') || (userChoice === "scissors" && computerChoice === "paper") || (userChoice === "paper" && computerChoice === "rock")) {
-        return result.innerText = `You won! You chose ${userChoice} and the computer chose ${computerChoice}.`
+        playerScore += 1;
+        roundResult = `You won the round! You chose ${userChoice} and the computer chose ${computerChoice}.`
     } else {
-        return result.innerText = `The computer won! The computer chose ${computerChoice} and you chose ${userChoice}.`
+        computerScore += 1;
+        roundResult = `The computer won the round! The computer chose ${computerChoice} and you chose ${userChoice}.`
+    }
+
+    if (playerScore === 5) {
+        result.innerText = `You won the game! The final score is Player Score: ${playerScore} Computer Score: ${computerScore}.`
+        playerScore = 0
+        computerScore = 0
+    } else if (computerScore === 5) {
+        result.innerText = `The computer won the game! The final score is Player Score: ${playerScore} Computer Score: ${computerScore}.`
+        playerScore = 0
+        computerScore = 0
+    } else {
+        result.innerText = `${roundResult} Player Score: ${playerScore} Computer Score: ${computerScore}.`
     }
 };
